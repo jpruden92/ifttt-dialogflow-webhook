578ccbd492d879c2a7b1b2088533bb5caf29dd0d
Jorge Prudencio <jorge.prudencio@sngular.team>
2019-02-10 13:49:40 +0100

    Fixes

--- libs/auth.js
+++ libs/auth.js
@@ -1,13 +1,16 @@
 const auth = require('basic-auth');
-const config = require('../config.json');
+const configController = require('./config.js');
 
-const admins = { [config.user]: { password: config.password }, };
+module.exports = (request, response, next) => {
+    const config = configController.getConfig();
+    const admins = { [config.user]: { password: config.password } };
 
-module.exports = function (request, response, next) {
-  var user = auth(request);
-  if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
-    response.set('WWW-Authenticate', 'Basic realm="example"');
-    return response.status(401).send();
-  }
-  return next();
+    const user = auth(request);
+
+    if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+        response.set('WWW-Authenticate', 'Basic realm="example"');
+        return response.status(401).send();
+    }
+
+    return next();
 };
